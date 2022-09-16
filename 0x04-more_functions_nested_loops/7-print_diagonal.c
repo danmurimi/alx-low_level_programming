98a5a9e62171ce8ff8dbcfc131b6855c93235342
danmurimi <danmurimi369@gmail.com>
2022-09-16 12:44:48 -0700

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal -> print line
+ * @n: integer params
+ */
+void print_diagonal(int n)
+{
+	int x;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (i = 0; i <= x; i++)
+				putchar(' ');
+			_putchar(92);
+			putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
